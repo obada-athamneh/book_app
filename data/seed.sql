67533cfc913b3e2fe313e3798653b2b1a4b588c9
Obada Athamneh <obadaobaida@gmail.com>
2021-04-06 23:04:20 +0300

    THis made by obada

---
+++ data/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO books(
+	author, title, isbn, image_url, description)
+	VALUES ('Sarah Brown', 'The Cat', 'ISBN_13 9781782408574', 'http://books.google.com/books/content?id=H3DNDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', 'Anyone with an interest in or love for cats, and all those who have owned a cat will find much to learn and enjoy in The Cat: A Natural History.');
+
+INSERT INTO books(
+    author, title, isbn, image_url, description)
+    VALUES ('Cynthia Petrie Smith', 'The Cat', 'OTHER MINN:31951D01743663J', 'http://books.google.com/books/content?id=ZBOVu7PoGxsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', null);
